8619ec72334542022b3b8875af6c6a486b2bdd66
Oleksandr Arman <o.arman1994@gmail.com>
2024-05-06 14:57:07 +0200

    Tasca P244-223 Android veure els meus chats

--- app/src/main/java/org/udg/pds/todoandroid/rest/TodoApi.java
+++ app/src/main/java/org/udg/pds/todoandroid/rest/TodoApi.java
@@ -1,6 +1,8 @@
 package org.udg.pds.todoandroid.rest;
 
+import org.udg.pds.todoandroid.entity.Chat;
 import org.udg.pds.todoandroid.entity.IdObject;
+import org.udg.pds.todoandroid.entity.Message;
 import org.udg.pds.todoandroid.entity.PaymentResponse;
 import org.udg.pds.todoandroid.entity.Task;
 import org.udg.pds.todoandroid.entity.User;
@@ -102,6 +104,13 @@ public interface TodoApi {
         @Part("preu") RequestBody preu,
         @Part("urlsToDel") List<String> urlsToDel,
         @Part List<MultipartBody.Part> files);
+
+    @GET("/chats/user/me")
+    Call<List<Chat>> getMeusChats();
+
+    @GET("chats/{chatId}/messages")
+    Call<List<Message>> getChatMessages(@Path("chatId") Long chatId);
+
 
 }
 
