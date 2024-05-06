699f65a3f82131630225223cce65e42b111e1d3d
Joan0248 <u1978883@campus.udg.edu>
2024-05-06 11:25:51 +0200

    Desenvolupament inicial poder afegir posts a preferits (encara no acaba de funcionar)

--- app/src/main/java/org/udg/pds/todoandroid/activity/PostDetallHomeActivity.java
+++ app/src/main/java/org/udg/pds/todoandroid/activity/PostDetallHomeActivity.java
@@ -7,6 +7,8 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,22 +31,37 @@ public class PostDetallHomeActivity extends AppCompatActivity {
 
     TodoApi mApiService;
 
+    Long post_id;
+    Boolean isFavourite;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_detall_home);
         mApiService = ((TodoApp) this.getApplication()).getAPI();
 
-
         this.crearHomePost();
-
+        findViewById(R.id.favorite_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView favoriteIcon = (ImageView) view;
+                if(isFavourite){
+                    isFavourite = false;
+                    changeFavourite(post_id, false);
+                    favoriteIcon.setImageResource(R.drawable.star_not_filled);
+                } else{
+                    isFavourite= true;
+                    changeFavourite(post_id, true);
+                    favoriteIcon.setImageResource(R.drawable.star_filled);
+                }
+            }
+        });
     }
 
     private void crearHomePost(){
 
         String postId = getIntent().getStringExtra("POST_ID");
         Call<Post> call = mApiService.getPostId(postId);
-
         call.enqueue(new Callback<Post>() {
             @Override
             public void onResponse(Call<Post> call, Response<Post> response) {
@@ -68,10 +85,16 @@ public class PostDetallHomeActivity extends AppCompatActivity {
         TextView preu = findViewById(R.id.tvPrice);
         TextView userPropietari = findViewById(R.id.tvUserName);
         ViewPager2 viewPager = findViewById(R.id.post_item_image);
+        ImageView favoriteIcon = findViewById(R.id.favorite_icon);
+
+        post_id = p.getId();
+        isFavourite(post_id);
+
+        if(isFavourite) favoriteIcon.setImageResource(R.drawable.star_filled);
+        else favoriteIcon.setImageResource(R.drawable.star_not_filled);
 
         User u = p.getUser();
         Long userID = p.getUserId();
-
         titol.setText(p.getTitol());
         descripcio.setText(p.getDescripcio());
         preu.setText(String.format(Locale.getDefault(), "$%.2f", p.getPreu()));
@@ -81,4 +104,43 @@ public class PostDetallHomeActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
     }
 
+    private void changeFavourite(Long post_id, boolean addToFavourites){
+        Call<Void> call = mApiService.changeFavourite(post_id, addToFavourites);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    if(addToFavourites) Toast.makeText(PostDetallHomeActivity.this, "Post afegit a preferits", Toast.LENGTH_SHORT).show();
+                    else Toast.makeText(PostDetallHomeActivity.this, "Post tret de preferits", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(PostDetallHomeActivity.this, "Error a l'hora de modificar el post de preferits", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(PostDetallHomeActivity.this, "Fallada a l'hora de modificar el post de preferits", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void isFavourite(Long post_id){
+        Call<Boolean> call = mApiService.isFavourite(post_id);
+        call.enqueue(new Callback<Boolean>() {
+            @Override
+            public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                if (response.isSuccessful()) {
+                    Toast.makeText(PostDetallHomeActivity.this, "Bool:" + response.body().toString(), Toast.LENGTH_SHORT).show();
+                    isFavourite = response.body();
+                } else {
+                    Toast.makeText(PostDetallHomeActivity.this, "Error a l'hora de saber si el post és de preferits", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Boolean> call, Throwable t) {
+                Toast.makeText(PostDetallHomeActivity.this, "Fallada a l'hora de saber si el post és de preferits", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
